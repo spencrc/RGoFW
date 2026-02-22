a0ff1c5a420ebe5915e978bbaa58d67f8b3eb5b7
spencrc <145073726+spencrc@users.noreply.github.com>
2026-02-22 00:54:06 -0800

    created wgpurgfw package

---
+++ wgpurgfw/surface_x11.go
@@ -0,0 +1,21 @@
+//go:build linux && !android && !wayland
+
+package wgpurgfw
+
+/*
+	#cgo linux LDFLAGS: -lX11 -lXrandr
+*/
+import "C"
+import (
+	"github.com/cogentcore/webgpu/wgpu"
+	"github.com/spencrc/RGoFW/rgfw"
+)
+
+func GetSurfaceDescriptor(w *rgfw.Window) *wgpu.SurfaceDescriptor {
+	return &wgpu.SurfaceDescriptor{
+		XlibWindow: &wgpu.SurfaceDescriptorFromXlibWindow{
+			Display: rgfw.GetDisplayX11(),
+			Window:  w.GetWindowX11(),
+		},
+	}
+}
